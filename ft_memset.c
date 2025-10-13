f42e3f5f5657c4930e3cea6775e14772ed1370c9
Azlan19 <omarazlan6@gmail.com>
2025-10-13 09:32:23 +0200

    (ADD) Started ft_memset

---
+++ ft_memset.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/13 09:23:56 by oazlan            #+#    #+#             */
+/*   Updated: 2025/10/13 09:30:39 by oazlan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*DESCRIPTION
+    The memset() function writes len bytes of value c (converted to an
+    unsigned char) to the string b.
+     
+RETURN VALUES
+    The memset() function returns its first argument.*/
+    
+
+#include "libft.h"
+
+void* ft_memset(void *b, int c, size_t len)
+{
+    
+}
+
+int main()
+{
+    
+}
