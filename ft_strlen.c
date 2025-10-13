56d0a8f88469d51a835eaf12c2157846b9fa2d3c
Azlan19 <omarazlan6@gmail.com>
2025-10-13 08:57:14 +0200

    (TEST) Test strlen with main

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,13 +6,13 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/12 19:37:18 by oazlan            #+#    #+#             */
-/*   Updated: 2025/10/12 20:02:29 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/10/13 08:55:55 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t strlen(const char *s)
+size_t ft_strlen(const char *s)
 {
     size_t i;
 
@@ -26,3 +26,10 @@ size_t strlen(const char *s)
 }
 
 
+// int main()
+// {
+//     char *string = "testing";
+//     printf("%zu\n", ft_strlen(string));
+    
+//     return 0;
+// }
