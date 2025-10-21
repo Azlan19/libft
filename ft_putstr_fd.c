5fdd4d4d917f733d00c289c16a4b20ebc83c3b90
Azlan19 <omarazlan6@gmail.com>
2025-10-21 19:17:48 +0200

    (ADD) Add ft_putstr_fd, need to complete

---
+++ ft_putstr_fd.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/21 19:12:30 by oazlan            #+#    #+#             */
+/*   Updated: 2025/10/21 19:16:48 by oazlan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+DESCRIPTION
+    Outputs the string ’s’ to the specified file
+    descriptor.
+
+RETURN VALUE
+    None.
+*/
+
+
+#include "libft.h"
+
+void ft_putstr_fd(char *s, int fd)
+{
+    int i;
+    while(s[i])
+    {
+        write()
+    }
+}
