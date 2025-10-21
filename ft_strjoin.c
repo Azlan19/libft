3dd5dd6b5683d0735e54aa3d5507eb7751fd7d12
Azlan19 <omarazlan6@gmail.com>
2025-10-21 13:53:41 +0200

    (FIX) Complete ft_strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 10:44:18 by oazlan            #+#    #+#             */
-/*   Updated: 2025/10/21 10:45:36 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/10/21 13:48:11 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,40 @@ RETURN VALUE
 
 #include "libft.h"
 
+int ft_strlen(char const *s)
+{
+    int i;
+
+    i = 0;
+    while(s[i])
+    {
+        i++;
+    }
+    return i;
+}
+
 char *ft_strjoin(char const *s1, char const *s2)
 {
+    int i;
+    int j;
+    int total_length;
+    char *ptr;
     
+    total_length = ft_strlen(s1) + ft_strlen(s2);
+
+    ptr = (char *)malloc((total_length * sizeof(char)) + 1);
+
+    if (!ptr)
+    i = 0;
+    while(s1[i])
+    {
+        ptr[i++] = s1[i];
+    }
+    j = 0;
+    while(s2[j])
+    {
+        ptr[i++] = s2[j++];
+    }
+    ptr[i] = '\0';
+    return ptr;    
 }
