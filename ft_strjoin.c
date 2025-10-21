1b7d12ed54e2a55144a69e936cccb7941f7e9058
Azlan19 <omarazlan6@gmail.com>
2025-10-21 10:46:51 +0200

    (ADD) Started ft_strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/21 10:44:18 by oazlan            #+#    #+#             */
+/*   Updated: 2025/10/21 10:45:36 by oazlan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* 
+DESCRIPTION
+    Allocates memory (using malloc(3)) and returns a
+    new string, which is the result of concatenating
+    ’s1’ and ’s2’.
+    
+RETURN VALUE
+    The new string.
+    NULL if the allocation fails.
+*/
+
+
+#include "libft.h"
+
+char *ft_strjoin(char const *s1, char const *s2)
+{
+    
+}
