65a325cb3dd3211593a282cc1ab2e5863e260186
Azlan19 <omarazlan6@gmail.com>
2025-10-21 16:46:14 +0200

    (ADD) Add ft_split. I am doing this later, I have PTSD from the piscine

---
+++ ft_split.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/21 16:44:52 by oazlan            #+#    #+#             */
+/*   Updated: 2025/10/21 16:45:36 by oazlan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+DESCRIPTION
+    Allocates memory (using malloc(3)) and returns an
+    array of strings obtained by splitting ’s’ using
+    the character ’c’ as a delimiter. The array must
+    end with a NULL pointer.
+
+RETURN VALUE
+    The array of new strings resulting from the split.
+    NULL if the allocation fails.
+*/
+
+
+
+char **ft_split(char const *s, char c)
+{
+    
+}
