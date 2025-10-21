ef9351d49236841e92e371a2dd1db68f796e64b0
Azlan19 <omarazlan6@gmail.com>
2025-10-21 16:50:00 +0200

    (ADD) Start ft_itoa

---
+++ ft_itoa.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/21 16:46:31 by oazlan            #+#    #+#             */
+/*   Updated: 2025/10/21 16:48:58 by oazlan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+DESCRIPTION
+    Allocates memory (using malloc(3)) and returns
+    a string representing the integer received as an
+    argument. Negative numbers must be handled.
+
+RETURN VALUE
+    The string representing the integer.
+    NULL if the allocation fails.
+*/
+
+#include "libft.h"
+
+char *ft_itoa(int n)
+{
+    
+}
