d4d3d081e593f652e5d268f7e579b603a852bc8e
Azlan19 <omarazlan6@gmail.com>
2025-10-21 14:00:10 +0200

    (ADD) Start ft_strtrim

---
+++ ft_strtrim.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/21 13:54:27 by oazlan            #+#    #+#             */
+/*   Updated: 2025/10/21 13:58:04 by oazlan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+DESCRIPTION
+    Allocates memory (using malloc(3)) and returns a
+    copy of ’s1’ with characters from ’set’ removed
+    from the beginning and the end.
+
+RETURN VALUE
+    The trimmed string.
+    NULL if the allocation fails.
+*/
+
+
+
+#include "libft.h"
+
+char *ft_strtrim(char const *s1, char const *set)
+{
+    
+}
