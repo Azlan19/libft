e66691fed3bfbef1833ee13d2d311326d56ff8da
Azlan19 <omarazlan6@gmail.com>
2025-10-22 20:51:38 +0200

    (FIX) Complete ft_putchar_fd

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 19:12:30 by oazlan            #+#    #+#             */
-/*   Updated: 2025/10/21 19:16:48 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/10/22 20:50:07 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,14 @@ RETURN VALUE
 
 
 #include "libft.h"
+#include <unistd.h>
 
 void ft_putstr_fd(char *s, int fd)
 {
     int i;
+    i = 0;
     while(s[i])
     {
-        write()
+        write(fd, &s[i++], 1);
     }
 }
