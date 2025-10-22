68b388509efd566c22773ac2eef270dfb9bdddf8
Azlan19 <omarazlan6@gmail.com>
2025-10-22 20:58:46 +0200

    (ADD & FIX) Complete ft_putendl_fd

---
+++ ft_putendl_fd.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/22 20:54:16 by oazlan            #+#    #+#             */
+/*   Updated: 2025/10/22 20:58:19 by oazlan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+DESCRIPTION
+    Outputs the string ’s’ to the specified file
+    descriptor followed by a newline.
+
+RETURN VALUE
+    None.
+*/
+
+
+#include "libft.h"
+#include <unistd.h>
+
+void ft_putstr_fd(char *s, int fd)
+{
+    int i;
+    i = 0;
+    while(s[i])
+    {
+        write(fd, &s[i++], 1);
+    }
+}
+
+void ft_putendl_fd(char *s, int fd)
+{
+    ft_putstr_fd(s, fd);
+    write(fd, "\n", 1); 
+}
