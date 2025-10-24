2e5dc2a8ea7073c9138f67d03a7a1e4f2e62e1e2
Omar Azlan <oazlan@2-C-5.42heilbronn.de>
2025-10-24 19:13:52 +0200

    (ADD) Add the correct strnstr function instead of strstr

---
+++ ft_strnstr.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 16:50:46 by oazlan            #+#    #+#             */
+/*   Updated: 2025/10/24 19:13:04 by oazlan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+DESCRIPTION
+	The strnstr() function locates the first occurrence of the null-terminated
+	string little in the string big, where not more than len characters are
+	searched. Characters that appear after a ‘\0’ character are not searched.
+	Since the strnstr() function is a FreeBSD specific API, it should only be
+	used when portability is not a concern.
+
+RETURN VALUES
+	If little is an empty string, big is returned; if little occurs nowhere in
+	big, NULL is returned; otherwise a pointer to the first character of the
+	first occurrence of little is returned.
+	*/
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	int			i;
+	int			j;
+	const char	*ptr;
+
+	ptr = 0;
+	if (*little == '\0')
+	{
+		return ((char *)big);
+	}
+	i = 0;
+	while (big[i] && i < len)
+	{
+		j = 0;
+		while (big[i + j] == little[j])
+		{
+			if (little[j + 1] == 0)
+			{
+				return ((char *)big + i);
+			}
+			j++;
+		}
+		i++;
+	}
+	return ((char *)ptr);
+}
