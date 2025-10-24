6c36cc6d3bf214103d36db292a3fe42a084d32a2
Omar Azlan <oazlan@2-C-5.42heilbronn.de>
2025-10-24 20:36:35 +0200

    (FIX) Fix and completed ft_memchr by removing a condition

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,14 +6,14 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/18 17:53:15 by oazlan            #+#    #+#             */
-/*   Updated: 2025/10/24 11:40:35 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/10/24 20:35:59 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*DESCRIPTION
 	The  memchr() function scans the initial n bytes of the memory area pointed
-    to by s for the first instance of c. Both c and the bytes of the memory area
-    pointed to by s are interpreted as unsigned char.
+	to by s for the first instance of c. Both c and the bytes of the memory area
+	pointed to by s are interpreted as unsigned char.
 
 RETURN VALUE
 	The memchr() function returns a pointer to the matching byte or NULL if the
@@ -27,17 +27,19 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	size_t			i;
 	unsigned char	*s_ptr;
 
+	if (n == 0)
+		return (NULL);
 	s_ptr = (unsigned char *)s;
 	i = 0;
-	while (s_ptr[i] && i < n)
+	while (i < n)
 	{
-		if (s_ptr[i] == (char)c)
+		if (s_ptr[i] == (unsigned char)c)
 		{
 			return (&s_ptr[i]);
 		}
 		i++;
 	}
-	if (s_ptr[i] == (char)c)
+	if (s_ptr[i] == (unsigned char)c)
 	{
 		return (&s_ptr[i]);
 	}
