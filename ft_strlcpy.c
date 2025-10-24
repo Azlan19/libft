d4fc979c13338a2fcd7e3cd7510eaee4ffb40ef0
Omar Azlan <oazlan@2-C-5.42heilbronn.de>
2025-10-24 20:20:13 +0200

    (FIX) Fix and complete ft_strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 13:26:50 by oazlan            #+#    #+#             */
-/*   Updated: 2025/10/24 11:52:09 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/10/24 20:18:20 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,18 @@
 	designed to be safer, more consistent, and less error prone replacements for
 	strncpy(3) and strncat(3). Unlike those functions, strlcpy() takes the full
 	size of the buffer (not just the length) and guarantees to NUL-terminate the
-	result (as long as size is larger than 0.  Note that a byte for the NUL
-    should be included in size. Also note that strlcpy() only operates on true
-    “C” strings. This means that for strlcpy() src must be NUL-terminated and
-    for strlcat() both src and dst must be NUL-terminated.
+	result (as long as size is larger than 0.)  Note that a byte for the NUL
+	should be included in size. Also note that strlcpy() only operates on true
+	“C” strings. This means that for strlcpy() src must be NUL-terminated and
+	for strlcat() both src and dst must be NUL-terminated.
 
 	The strlcpy() function copies up to size - 1 characters from the
-    NUL-terminated string src to dst, NUL-terminating the result.
+	NUL-terminated string src to dst, NUL-terminating the result.
 
 
 RETURN VALUES
 	The strlcpy() and strlcat() functions return the total length of the string
-    they tried to create. For strlcpy() that means the length of src.
+	they tried to create. For strlcpy() that means the length of src.
 
 */
 
@@ -42,26 +42,30 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	{
 		j++;
 	}
-	i = 0;
-	while (dst[i] && i < (size - 1))
+	if (size > 0)
 	{
-		dst[i] = src[i];
-		i++;
+		i = 0;
+		while (src[i] && i < (size - 1))
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
 	}
 	return (j);
 }
 
 // int main()
 // {
-//     char string1[] = "Hello World";
-//     char string2[20];
+//     char src[] = "";
+//     char dest[20];
 
-//     printf("string1 = %s\n", string1);
-//     printf("string2 = %s\n\n", string2);
+//     printf("src = %s\n", src);
+//     printf("dest = %s\n\n", dest);
 
-//     printf("size_t = %li\n", ft_strlcpy(string2, string1, 9));
-//     printf("string1 = %s\n", string1);
-//     printf("string2 = %s\n\n", string2);
+//     printf("size_t = %li\n", ft_strlcpy(dest, src, 9));
+//     printf("src = %s\n", src);
+//     printf("dest = %s\n\n", dest);
 
 //     return (0);
 // }
