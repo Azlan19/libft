86e00ce11c4d8e72e16930ec50642ac32cf458a4
Omar Azlan <oazlan@2-D-2.42heilbronn.de>
2025-10-24 12:51:24 +0200

    (FIX) ft_itoa works now without seg faulting

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,15 +6,14 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 16:46:31 by oazlan            #+#    #+#             */
-/*   Updated: 2025/10/24 11:25:40 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/10/24 12:50:10 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
 DESCRIPTION
-	Allocates memory (using malloc(3)) and returns
-	a string representing the integer received as an
-	argument. Negative numbers must be handled.
+	Allocates memory (using malloc(3)) and returns a string representing the
+	integer received as an argument. Negative numbers must be handled.
 
 RETURN VALUE
 	The string representing the integer.
@@ -41,54 +40,61 @@ long long	ft_intlen(long long n)
 	return (count);
 }
 
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	size_t			i;
+	unsigned char	*ptr;
+
+	ptr = (unsigned char *)malloc(nmemb * size);
+	i = 0;
+	if (!ptr || nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	while (i < nmemb * size)
+	{
+		ptr[i] = 0;
+		i++;
+	}
+	return (ptr);
+}
+
 char	*ft_itoa(int n)
 {
 	long long	intlen;
 	char		*ptr;
 	int			sign;
 	long long	big_n;
-	int			stepcount;
 
 	big_n = n;
 	intlen = ft_intlen(big_n);
-	printf("Int length = %lld\n", intlen);
-	ptr = (char *)malloc((intlen + 1) * sizeof(char));
+	ptr = (char *)ft_calloc(sizeof(char), intlen + 1);
 	if (!ptr)
-	{
 		return (NULL);
-	}
 	sign = 0;
 	if (big_n < 0)
 	{
 		sign = 1;
 		big_n = -big_n;
 	}
-	printf("Sign = %d\n\n", sign);
-	ptr[intlen] = '\0';
-	stepcount = 1;
 	while (big_n > 0)
 	{
 		ptr[intlen - 1] = (big_n % 10) + '0';
 		big_n = big_n / 10;
-		printf("Step %d:\n", stepcount);
-		printf("n = %lld\n\n", big_n);
 		intlen--;
-		stepcount++;
 	}
 	if (sign == 1)
-	{
 		ptr[intlen - 1] = '-';
-	}
-	printf("Number = '%s'\n", ptr);
-	return (NULL);
+	return (ptr);
 }
 
-int	main(void)
-{
-	long long number = -2147483648;
+// int	main(void)
+// {
+// 	long long	number;
 
-	printf("\n[NUMBER = %lld]\n\n", number);
-	ft_itoa(number);
-
-	return (0);
-}
+// 	// number = -123;
+// 	number = -2147483648;
+// 	printf("\n[NUMBER = %lld]\n", number);
+// 	printf("\n[STRING = %s]\n\n", ft_itoa(number));
+// 	return (0);
+// }
