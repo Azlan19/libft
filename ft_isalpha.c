e2a06f87eb9ef8025d67c40dc9a1c4ffb7f04bae
Omar Azlan <oazlan@2-D-2.42heilbronn.de>
2025-10-24 13:09:54 +0200

    (ADD) Added header file

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,9 +6,11 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 21:34:10 by oazlan            #+#    #+#             */
-/*   Updated: 2025/10/24 11:45:48 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/10/24 12:58:48 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	ft_isaplha(int c)
 {
