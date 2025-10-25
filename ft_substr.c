d888d8ddedd7a10a2fc326cda7816ce74fbcbe2e
Omar Azlan <oazlan@2-C-9.42heilbronn.de>
2025-10-25 18:15:11 +0200

    (EDIT) Checkpoint for this function. idk why it segfaults.

--- ft_substr.c
+++ ft_substr.c
@@ -6,15 +6,15 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/18 20:19:10 by oazlan            #+#    #+#             */
-/*   Updated: 2025/10/24 16:25:53 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/10/25 18:12:57 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
 DESCRIPTION
 	Allocates memory (using malloc(3)) and returns a substring from the
-    string ’s’. The substring starts at index ’start’ and has a maximum length
-    of ’len’.
+	string ’s’. The substring starts at index ’start’ and has a maximum length
+	of ’len’.
 
 RETURN VALUE
 	The substring. NULL if the allocation fails.
@@ -27,21 +27,20 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	i;
 	char	*ptr;
 
-	i = 0;
-	if (!s[i])
-	{
+	if (!s)
 		return (NULL);
-	}
 	if (ft_strlen(s) < start)
-	{
 		return (NULL);
-	}
+	if (len > ft_strlen(s + start))
+		len = ft_strlen(s + start);
 	ptr = (char *)ft_calloc(len + 1, sizeof(char));
+	if (!ptr)
+		return (NULL);
+	i = 0;
 	while (i < len)
 	{
 		ptr[i] = s[start + i];
 		i++;
 	}
-	ptr[i] = '\0';
 	return (ptr);
 }
