3202f219f1337ced678d551985a97cb649c50c66
Omar Azlan <oazlan@2-C-9.42heilbronn.de>
2025-10-27 12:40:59 +0100

    (ADD) Start ft_lstnew

---
+++ ft_lstnew.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/27 12:38:02 by oazlan            #+#    #+#             */
+/*   Updated: 2025/10/27 12:40:33 by oazlan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+DESCRIPTION
+	Allocates memory (using malloc(3)) and returns
+	a new node. The ’content’ member variable is
+	initialized with the given parameter ’content’.
+	The variable ’next’ is initialized to NULL.
+
+RETURN VALUE
+	A pointer to the new node.
+*/
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	(void)content;
+}
