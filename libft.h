f4a2a958ecec658651e260e7bdf33c3694ba9f63
Omar Azlan <oazlan@2-C-9.42heilbronn.de>
2025-10-27 12:58:14 +0100

    (ADD) Add bonus functions to header file

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 11:53:15 by oazlan            #+#    #+#             */
-/*   Updated: 2025/10/27 12:31:42 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/10/27 12:53:52 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,5 +66,11 @@ typedef struct s_list
 	void			*content;
 	struct s_list	*next;
 }					t_list;
+
+t_list				*ft_lstnew(void *content);
+void				ft_lstadd_front(t_list **lst, t_list *new);
+int					ft_lstsize(t_list *lst);
+t_list				*ft_lstlast(t_list *lst);
+void				ft_lstadd_back(t_list **lst, t_list *new);
 
 #endif
