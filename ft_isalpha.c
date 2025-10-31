e1e0931be242b4c035296bd4c0b968e363354ca0
Azlan19 <omarazlan6@gmail.com>
2025-10-31 16:44:46 +0100

    (MAIN) Complete main for ft_isalpha

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,9 +6,20 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 21:34:10 by oazlan            #+#    #+#             */
-/*   Updated: 2025/10/24 17:40:13 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/10/31 16:44:23 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+DESCRIPTION
+	The isalpha() function tests for any character for which isupper(3) or
+	islower(3) is true. The value of the argument must be representable as an
+	unsigned char or the value of EOF.
+
+RETURN VALUES
+	The isalpha() function returns zero if the character tests false and
+	returns non-zero if the character tests true.
+*/
 
 #include "libft.h"
 
@@ -21,14 +32,16 @@ int	ft_isalpha(int c)
 	return (1);
 }
 
-// #include <stdio.h>
-
 // int	main(void)
 // {
-// 	printf("%i\n", ft_isaplha('A'));
-// 	printf("%i\n", ft_isaplha('Z'));
-// 	printf("%i\n", ft_isaplha('a'));
-// 	printf("%i\n", ft_isaplha('z'));
-// 	printf("%i\n", ft_isaplha('1'));
+// 	printf("\n[1 = TRUE, 0 = FALSE]\n\n");
+// 	printf("A is %i\n", ft_isalpha('A'));
+// 	printf("Z is %i\n", ft_isalpha('Z'));
+// 	printf("a is %i\n", ft_isalpha('a'));
+// 	printf("z is %i\n", ft_isalpha('z'));
+// 	printf("1 is %i\n", ft_isalpha('1'));
+// 	printf("9 is %i\n", ft_isalpha('9'));
+// 	printf("/ is %i\n", ft_isalpha('/'));
+// 	printf(") is %i\n\n", ft_isalpha(')'));
 // 	return (0);
 // }
