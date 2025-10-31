415c9655ee46f9294340625668c74643875976d5
Omar Azlan <oazlan@2-A-5.42heilbronn.de>
2025-10-31 19:55:05 +0100

    (NORMINETTE) Fix norminette for ft_memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,22 +6,23 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/18 18:11:17 by oazlan            #+#    #+#             */
-/*   Updated: 2025/10/31 19:51:33 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/10/31 19:53:06 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*DESCRIPTION
 	The memcmp() function compares the first n bytes (each interpreted as
-    unsigned char) of the memory areas s1 and s2.
+	unsigned char) of the memory areas s1 and s2.
 
 RETURN VALUE
 	The memcmp() function returns an integer less than, equal to, or greater
-    than zero if the first n bytes of s1 is found, respectively, to be less than,
+	than zero if the first n bytes of s1 is found, respectively,
+		to be less than,
 	to match, or be greater than the first n bytes of s2.
 
 	For a nonzero return value, the sign is determined by the sign of the
-    difference between the first pair of bytes (interpreted as unsigned char)
-    that differ in s1 and s2.
+	difference between the first pair of bytes (interpreted as unsigned char)
+	that differ in s1 and s2.
 
 	If n is zero, the return value is zero.
 */
@@ -53,8 +54,6 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 // 	char string1[] = "Have a lAugh";
 // 	char string2[] = "Have a laugh";
 // 	size_t n_bytes = 9;
-
-	
 
 // 	printf("\nBEFORE:\n");
 // 	printf("[STRING_1     = %s]\n", string1);
