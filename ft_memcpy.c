c22f3aafb1a317ddb404fbe91524819b749dee6b
Omar Azlan <oazlan@2-A-5.42heilbronn.de>
2025-10-31 20:06:02 +0100

    (MAIN) Complete main for ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 14:05:40 by oazlan            #+#    #+#             */
-/*   Updated: 2025/10/27 20:59:56 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/10/31 20:03:57 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,3 +42,22 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	}
 	return (dest);
 }
+
+// int main()
+// {
+// 	char dest[] = "What";
+// 	char src[] = "Phew";
+// 	size_t n_bytes = 2;
+
+// 	printf("\nBEFORE:\n");
+// 	printf("[DESTINATION  = %s]\n", dest);
+// 	printf("[SOURCE       = %s]\n", src);
+// 	printf("[NO. OF BYTES = %zu]\n", n_bytes);
+
+// 	printf("\nAFTER:\n");
+// 	printf("[ft_memcpy   = %s]\n\n", (char *)ft_memcpy(dest, src, n_bytes));
+// 	printf("[DESTINATION = %s]\n", dest);
+// 	printf("[SOURCE      = %s]\n\n", src);
+
+// 	return (0);
+// }
