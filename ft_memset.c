b7c430c1dcd0c573407de17a447db9d02686bc6a
Omar Azlan <oazlan@2-B-9.42heilbronn.de>
2025-11-01 01:35:25 +0100

    (MAIN) Complete main for ft_memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 09:23:56 by oazlan            #+#    #+#             */
-/*   Updated: 2025/10/24 11:26:04 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/11/01 01:35:03 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,34 +34,19 @@ void	*ft_memset(void *b, int c, size_t len)
 	return (ptr);
 }
 
-// #include <stdio.h>
-// #include <string.h>
-
 // int	main(void)
 // {
-// 	char str1[20] = "Hello, World!";
-// 	char str2[20] = "Hello, World!";
+// 	char	string[] = "Abracadabra";
+// 	char	character = 'h';
+// 	size_t	n_bytes = 4;
 
-// 	printf("Before memset:\n");
-// 	printf("str1: %s\n", str1);
-// 	printf("str2: %s\n\n", str2);
+// 	printf("\nBEFORE:\n");
+// 	printf("[STRING       = %s]\n", string);
+// 	printf("[CHAR         = %c]\n", character);
+// 	printf("[NO. OF BYTES = %zu]\n", n_bytes);
 
-// 	// Apply standard memset
-// 	memset(str1 + 7, '*', 5);
-
-// 	// Apply your ft_memset
-// 	ft_memset(str2 + 7, '*', 5);
-
-// 	printf("After memset:\n");
-// 	printf("str1: %s\n", str1);
-// 	printf("After ft_memset:\n");
-// 	printf("str2: %s\n\n", str2);
-
-// 	// Compare the results
-// 	if (memcmp(str1, str2, sizeof(str1)) == 0)
-// 		printf("✅ ft_memset works correctly!\n");
-// 	else
-// 		printf("❌ ft_memset does NOT match memset.\n");
+// 	printf("\nAFTER:\n");
+// 	printf("[STRING = %s]\n\n", (char *)ft_memset(string, character, n_bytes));
 
 // 	return (0);
 // }
