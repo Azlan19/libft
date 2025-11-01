0eab7efa6a757246ede502102dc2345fb2d6dc0e
Omar Azlan <oazlan@2-B-9.42heilbronn.de>
2025-11-01 02:22:44 +0100

    (MAIN) Complete main for ft_striteri

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 19:28:19 by oazlan            #+#    #+#             */
-/*   Updated: 2025/10/27 09:37:36 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/11/01 02:22:09 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,3 +34,26 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 		i++;
 	}
 }
+
+// void	upper_every_even(unsigned int i, char *s)
+// {
+// 	if (i % 2 == 0)
+// 	{
+// 		*s = *s - 32;
+// 	}
+// }
+
+// int main()
+// {
+// 	char string[] = "alphabet";
+
+// 	printf("\nBEFORE:\n");
+// 	printf("[STRING = %s]\n", string);
+
+// 	ft_striteri(string, upper_every_even);
+
+// 	printf("\nAFTER:\n");
+// 	printf("[STRING = %s]\n\n", string);
+
+// 	return (0);
+// }
