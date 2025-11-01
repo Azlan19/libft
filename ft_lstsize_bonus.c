af234af0f7818e850d7f748278059390b3308d87
Omar Azlan <oazlan@2-B-9.42heilbronn.de>
2025-11-01 03:58:51 +0100

    (MAIN) Complete main for ft_lstsize

--- ft_lstsize_bonus.c
+++ ft_lstsize_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*   ft_lstsize_bonus.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 12:42:52 by oazlan            #+#    #+#             */
-/*   Updated: 2025/10/27 14:44:28 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/11/01 03:58:26 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,3 +32,23 @@ int	ft_lstsize(t_list *lst)
 	}
 	return (i);
 }
+
+// int	main(void)
+// {
+// 	t_list *list = NULL;
+// 	t_list *node1 = ft_lstnew("Hello");
+// 	t_list *node2 = ft_lstnew("World");
+// 	t_list *node3 = ft_lstnew("!");
+
+// 	printf("\n[node 1 = '%s']\n", (char*)node1->content);
+// 	printf("[node 2 = '%s']\n", (char*)node2->content);
+// 	printf("[node 3 = '%s']\n", (char*)node3->content);
+
+// 	ft_lstadd_back(&list, node1);
+// 	ft_lstadd_back(&list, node2);
+// 	ft_lstadd_back(&list, node3);
+
+// 	printf("\n[ft_lstsize = %d]\n\n", ft_lstsize(list));
+
+// 	return (0);
+// }
