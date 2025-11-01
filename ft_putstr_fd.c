d5fe4056913e13e311656a66fb5e64e626717645
Omar Azlan <oazlan@2-B-9.42heilbronn.de>
2025-11-01 01:49:22 +0100

    (MAIN) Complete main for ft_putstr_fd

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 19:12:30 by oazlan            #+#    #+#             */
-/*   Updated: 2025/10/24 16:21:50 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/11/01 01:49:01 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,3 +31,15 @@ void	ft_putstr_fd(char *s, int fd)
 		write(fd, &s[i++], 1);
 	}
 }
+
+// int main()
+// {
+// 	char string[] = "This works";
+// 	int file_descriptior = 1;
+
+// 	printf("\n");
+// 	ft_putstr_fd(string, file_descriptior);
+// 	printf("\n\n");
+
+// 	return (0);
+// }
