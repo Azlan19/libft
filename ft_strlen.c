a279911adbbc2fac36cb643d8d9dd0cffe95a9d6
Omar Azlan <oazlan@2-B-9.42heilbronn.de>
2025-11-01 02:44:21 +0100

    (MAIN) Complete main for ft_strlen

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,9 +6,19 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/12 19:37:18 by oazlan            #+#    #+#             */
-/*   Updated: 2025/10/24 11:26:37 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/11/01 02:43:56 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+DESCRIPTION
+	The strlen() function calculates the length of the string pointed to by s,
+	excluding the terminating null byte ('\0').
+
+RETURN VALUE
+	The strlen() function returns the number of bytes in the string pointed to
+	by s.
+*/
 
 #include "libft.h"
 
@@ -26,8 +36,8 @@ size_t	ft_strlen(const char *s)
 
 // int main()
 // {
-//     char *string = "testing";
-//     printf("%zu\n", ft_strlen(string));
+//     char string[] = "testing";
+//     printf("\n'%s' has '%zu' characters\n\n", string, ft_strlen(string));
 
 //     return (0);
 // }
