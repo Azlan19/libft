7272d49a56f1ef83f8907f419ed9b1a3df6f0485
Omar Azlan <oazlan@2-B-9.42heilbronn.de>
2025-11-01 03:33:42 +0100

    (MAIN) Complete main for ft_lstadd_back_bonus

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 12:46:02 by oazlan            #+#    #+#             */
-/*   Updated: 2025/10/27 19:08:24 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/11/01 03:33:03 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,3 +36,24 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	last = ft_lstlast(*lst);
 	last->next = new;
 }
+
+// int	main(void)
+// {
+// 	t_list *list = NULL;
+// 	t_list *node1 = ft_lstnew("Hello");
+// 	t_list *node2 = ft_lstnew("World");
+// 	t_list *node3 = ft_lstnew("!");
+
+// 	ft_lstadd_back(&list, node1);
+// 	ft_lstadd_back(&list, node2);
+// 	ft_lstadd_back(&list, node3);
+
+// 	int i = 1;
+// 	while (list)
+// 	{
+// 		printf("[NODE_%d = '%s']\n", i, (char *)list->content);
+// 		list = list->next;
+// 		i++;
+// 	}
+// 	return (0);
+// }
